0b2bca5292466f206c60d10b08b706fcc4ecb214
painextazzy <painextazzy@gmail.com>
2026-05-06 22:13:19 +0300

    fix

--- backend/src/main/java/com/restaurant/entity/Commande.java
+++ backend/src/main/java/com/restaurant/entity/Commande.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class Commande {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)  // ← Changé pour Railway (MySQL)
+    @GeneratedValue(strategy = GenerationType.AUTO) // ← Changé pour Railway (MySQL)
     private Long id;
 
     @Column(name = "numero_facture")
