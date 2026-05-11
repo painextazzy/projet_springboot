acd03e50d2247ec118e8229440e77af41a90a25f
painextazzy <painextazzy@gmail.com>
2026-05-11 22:37:13 +0300

    fix

--- frontend/src/public/Accueil.jsx
+++ frontend/src/public/Accueil.jsx
@@ -179,7 +179,7 @@ export default function Accueil() {
                     onClick={() => setShowPassword(!showPassword)}
                   >
                     <span className="material-symbols-outlined text-xl">
-                      {showPassword ? "visibility_off" : "visibility_on"}
+                      {showPassword ? "visibility_off" : "visibility"}
                     </span>
                   </button>
                 </div>
